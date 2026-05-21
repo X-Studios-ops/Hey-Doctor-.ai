def8de3c59b93822b69eb4fcd02f68b088ffe3c5
X-Studios-ops <som423428@gmail.com>
2026-05-21 12:53:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ if "premium_licensed" not in st.session_state:
 # 3. CORE AI ENGINE & SECURITY GATEWAY (GEMINI 2.5 FLASH)
 # ============================================================================
 # Apni working Google AI Studio API Key yahan check kar lena
-GEMINI_API_KEY = "AIzaSyA_Eg-EhCaUrQF5e4c0f3M-Nge7ssNeQmE" 
+GEMINI_API_KEY = "AIzaSyAkZWNo4QSUgH8pE1YX_c2DUOmO9zkBJEs" 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
 GOD_MODE_SYSTEM_INSTRUCTION = """
